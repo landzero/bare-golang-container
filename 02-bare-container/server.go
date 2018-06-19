2331762ae26d9d2c38dfd9ae9ad18dc65f1d9bf7
etanaru <etanaru@landzero.net>
2018-06-19 18:59:30 +0800

    prove bare-go can resolve DNS

--- 02-bare-container/server.go
+++ 02-bare-container/server.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, %s", "world")
+		if res, err := http.Get("http://httpbin.org/get"); err == nil {
+			io.Copy(w, res.Body)
+			res.Body.Close()
+		} else {
+			fmt.Fprintf(w, "hello, %s", "world")
+		}
 	})
 	http.ListenAndServe(":8080", nil)
 }
